6ba7b39b386f51a908f27ac33f454b94d01ea378
inudola <es.futures26@gmail.com>
2018-11-10 19:59:35 +0700

    Merubah Deskripsi App & Index

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Buka <code>src/App.js</code> simpan dan muat ulang.
           </p>
           <a
             className="App-link"
